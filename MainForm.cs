9ea66ca62fe58a752faa65c61d19424aba97b429
yifan chen <865636335@qq.com>
2023-11-10 00:26:00 +0800

    s

--- MainForm.cs
+++ MainForm.cs
@@ -1,17 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Diagnostics;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Diagnostics;
 using PacketDotNet;
 using SharpPcap;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ToolBar;
 
 namespace nps
 {
@@ -19,12 +8,10 @@ namespace nps
     {
         private CaptureDeviceList devices;
         private ICaptureDevice? selectedDevice = null;
-        private PacketArrivalEventHandler arrivalEventHandler;
-        private List<RawCapture> capturedPackets = new List<RawCapture>();
+        private List<RawCapture> capturedPackets = new();
 
-        private object QueueLock = new object();
         private System.Threading.Thread backgroundThread;
-        private bool BackgroundThreadStop;
+        private bool backgroundThreadStop;
 
         public MainForm()
         {
@@ -43,22 +30,22 @@ namespace nps
             listPacket.View = View.Details;
             listPacket.Columns.Add("No", 50, HorizontalAlignment.Left);
             listPacket.Columns.Add("Time", 150, HorizontalAlignment.Left);
-            listPacket.Columns.Add("Source", 140, HorizontalAlignment.Left);
-            listPacket.Columns.Add("Destination", 140, HorizontalAlignment.Left);
+            listPacket.Columns.Add("Source", 150, HorizontalAlignment.Left);
+            listPacket.Columns.Add("Destination", 150, HorizontalAlignment.Left);
             listPacket.Columns.Add("Length", 80, HorizontalAlignment.Left);
-            listPacket.Columns.Add("网络层协议", 90, HorizontalAlignment.Left);
-            listPacket.Columns.Add("传输层协议", 90, HorizontalAlignment.Left);
-            listPacket.Columns.Add("应用层协议", 90, HorizontalAlignment.Left);
+            listPacket.Columns.Add("网络层协议", 100, HorizontalAlignment.Left);
+            listPacket.Columns.Add("传输层协议", 100, HorizontalAlignment.Left);
+            listPacket.Columns.Add("应用层协议", 100, HorizontalAlignment.Left);
 
             listFilter.View = View.Details;
             listFilter.Columns.Add("No", 50, HorizontalAlignment.Left);
             listFilter.Columns.Add("Time", 150, HorizontalAlignment.Left);
-            listFilter.Columns.Add("Source", 140, HorizontalAlignment.Left);
-            listFilter.Columns.Add("Destination", 140, HorizontalAlignment.Left);
+            listFilter.Columns.Add("Source", 150, HorizontalAlignment.Left);
+            listFilter.Columns.Add("Destination", 150, HorizontalAlignment.Left);
             listFilter.Columns.Add("Length", 80, HorizontalAlignment.Left);
-            listFilter.Columns.Add("网络层协议", 90, HorizontalAlignment.Left);
-            listFilter.Columns.Add("传输层协议", 90, HorizontalAlignment.Left);
-            listFilter.Columns.Add("应用层协议", 90, HorizontalAlignment.Left);
+            listFilter.Columns.Add("网络层协议", 100, HorizontalAlignment.Left);
+            listFilter.Columns.Add("传输层协议", 100, HorizontalAlignment.Left);
+            listFilter.Columns.Add("应用层协议", 100, HorizontalAlignment.Left);
 
             comboBox1.Items.Add("所有");
             comboBox1.Items.Add("IPv4");
@@ -67,6 +54,8 @@ namespace nps
             comboBox2.Items.Add("Tcp");
             comboBox2.Items.Add("Udp");
             comboBox3.Items.Add("所有");
+
+            btnStop.Enabled = false;
         }
 
         private void btnDeviceDetails_Click(object sender, EventArgs e)
@@ -75,27 +64,28 @@ namespace nps
             MessageBox.Show(dev.ToString(), "info");
         }
 
+        private PacketArrivalEventHandler arrivalEventHandler;
         private void btnStart_Click(object sender, EventArgs e)
         {
             //开始捕获,创建线程backgroundThread
             capturedPackets = new List<RawCapture>();
-            BackgroundThreadStop = false;
-            backgroundThread = new System.Threading.Thread(BackgroundThread);
+            backgroundThread = new Thread(BackgroundThread);
+            backgroundThreadStop = false;
             backgroundThread.Start();
+            
 
             selectedDevice = devices[selectDevices.SelectedIndex];
-            arrivalEventHandler = new PacketArrivalEventHandler(device_OnPacketArrival);
+            arrivalEventHandler = new PacketArrivalEventHandler(Packet_ArrivalEventHandler);
             selectedDevice.OnPacketArrival += arrivalEventHandler;
             selectedDevice.Open();
-            selectedDevice.Filter = filter;
             selectedDevice.StartCapture();
 
             btnStart.Enabled = false;
             btnStop.Enabled = true;
         }
 
-
-        void device_OnPacketArrival(object sender, PacketCapture e)
+        private readonly object QueueLock = new();
+        void Packet_ArrivalEventHandler(object sender, PacketCapture e)
         {
             lock (QueueLock)
             {
@@ -103,11 +93,10 @@ namespace nps
             }
 
         }
-        private List<string> proto = new List<string>();
+        private List<string> protocols = new();
 
         private void BackgroundThread()
         {
-            Debug.WriteLine("线程开启");
             Control.CheckForIllegalCrossThreadCalls = false;
             listPacket.Items.Clear();
             var count = 0;
@@ -122,17 +111,17 @@ namespace nps
                 }
                 if (shouldSleep)
                 {
-                    if (BackgroundThreadStop == true)
+                    if (backgroundThreadStop == true)
                         break;
                     else
-                        System.Threading.Thread.Sleep(50);
+                        Thread.Sleep(50);
                 }
                 else
                 {
                     var rawPacket = capturedPackets[count];
-                    Packet packet = PacketDotNet.Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+                    Packet packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
 
-                    ListViewItem item = new ListViewItem();
+                    ListViewItem item = new();
                     //NO
                     item.SubItems[0].Text = count.ToString();
                     //Time
@@ -175,25 +164,24 @@ namespace nps
                     }
 
 
-                    item.SubItems.Add(judgeProtocol(packet)); //应用层协议
+                    item.SubItems.Add(JudgeProtocol(packet)); //应用层协议
                     listPacket.Items.Add(item);
                     count++;
 
-                    string protocol = judgeProtocol(packet);
+                    string protocol = JudgeProtocol(packet);
                     if (protocol != null && protocol != "")
                     {
-                        if (!proto.Contains(protocol))
+                        if (!protocols.Contains(protocol))
                         {
-                            proto.Add(protocol);
+                            protocols.Add(protocol);
                             comboBox3.Items.Add(protocol);
                         }
                     }
                 }
             }
-            Debug.WriteLine("线程结束");
         }
 
-        private string judgeProtocol(Packet packet)
+        private static string JudgeProtocol(Packet packet)
         {
             String protocol = "";
             TcpPacket tcpPacket = packet.Extract<PacketDotNet.TcpPacket>();
@@ -218,10 +206,6 @@ namespace nps
 
         private void btnStop_Click(object sender, EventArgs e)
         {
-            if (selectedDevice == null)
-            {
-                return;
-            }
             selectedDevice.StopCapture();
             selectedDevice.Close();
             selectedDevice.OnPacketArrival -= arrivalEventHandler;
@@ -229,18 +213,16 @@ namespace nps
             btnStart.Enabled = true;
             btnStop.Enabled = false;
 
-            BackgroundThreadStop = true;
+            backgroundThreadStop = true;
             //等待线程完成执行
             backgroundThread.Join();
         }
-
-        private string filter = "";
 
         private void btnFilterStart_Click(object sender, EventArgs e)
         {
             //确定筛选
             listFilter.Items.Clear();
-            update_listFilter();
+            Update_listFilter();
             listPacket.Visible = false;
             listFilter.Visible = true;
         }
@@ -252,8 +234,8 @@ namespace nps
             listFilter.Visible = false;
         }
 
-        private List<RawCapture> filterRawPacket = new List<RawCapture>();
-        private void update_listFilter()
+        private List<RawCapture> filterRawPacket = new();
+        private void Update_listFilter()
         {
             string selected1 = "";
             string selected2 = "";
@@ -268,7 +250,7 @@ namespace nps
             }
             if (comboBox3.SelectedIndex > 0)
             {
-                selected3 = proto[comboBox3.SelectedIndex - 1];
+                selected3 = protocols[comboBox3.SelectedIndex - 1];
             }
 
             filterRawPacket = new List<RawCapture>();
@@ -280,10 +262,8 @@ namespace nps
             for (int i = 0; i < count; i++)
             {
                 RawCapture rawPacket = capturedPackets[i];
-                Packet packet = PacketDotNet.Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+                Packet packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
                 EthernetPacket eth = (EthernetPacket)packet;
-                var tcpPacket = packet.Extract<PacketDotNet.TcpPacket>();
-                var udpPacket = packet.Extract<PacketDotNet.UdpPacket>();
 
                 if (selected1 != "")
                 {
@@ -301,7 +281,7 @@ namespace nps
                 }
                 if (selected3 != "")
                 {
-                    if (selected3 != judgeProtocol(packet))
+                    if (selected3 != JudgeProtocol(packet))
                         continue;
                 }
 
@@ -337,18 +317,19 @@ namespace nps
             }
         }
 
+        //调用一些库函数 串联起来
         private void show_detail()
         {
             RawCapture rawPacket = capturedPackets[select_index];
-            Packet packet = PacketDotNet.Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
+            Packet packet = Packet.ParsePacket(rawPacket.LinkLayerType, rawPacket.Data);
             EthernetPacket eth = (EthernetPacket)packet;
-            var tcpPacket = packet.Extract<PacketDotNet.TcpPacket>();
-            var udpPacket = packet.Extract<PacketDotNet.UdpPacket>();
+            var tcpPacket = packet.Extract<TcpPacket>();
+            var udpPacket = packet.Extract<UdpPacket>();
 
             int count = 0;
             treeView1.BeginUpdate();
             treeView1.Nodes.Clear();
-            treeView1.Nodes.Add("原始数据包:" + select_index.ToString());
+            treeView1.Nodes.Add("原始数据包:");
             treeView1.Nodes[count].Nodes.Add("链路层类型：" + rawPacket.LinkLayerType.ToString());
             treeView1.Nodes[count].Nodes.Add("捕获时间：" + rawPacket.Timeval.Date.ToLocalTime().TimeOfDay);
             treeView1.Nodes[count].Nodes.Add("长度：" + rawPacket.Data.Length.ToString());
@@ -361,7 +342,7 @@ namespace nps
             count++;
 
             treeView1.Nodes.Add("网络层");
-            if (eth.PayloadPacket is PacketDotNet.ArpPacket)
+            if (eth.PayloadPacket is ArpPacket)
             {
                 ArpPacket arppacket = (ArpPacket)eth.PayloadPacket;
                 treeView1.Nodes[count].Nodes.Add("协议类型：ARP");
@@ -375,7 +356,7 @@ namespace nps
 
             if (eth.PayloadPacket is IPPacket)
             {
-                IPPacket ippacket = (PacketDotNet.IPPacket)eth.PayloadPacket;
+                IPPacket ippacket = (IPPacket)eth.PayloadPacket;
                 treeView1.Nodes[count].Nodes.Add("协议类型：" + eth.Type.ToString());
                 treeView1.Nodes[count].Nodes.Add("数据包长度：" + ippacket.TotalLength);
                 treeView1.Nodes[count].Nodes.Add("版本：" + ippacket.Version);
@@ -385,9 +366,9 @@ namespace nps
                 treeView1.Nodes[count].Nodes.Add("生存时间：" + ippacket.TimeToLive.ToString());
                 treeView1.Nodes[count].Nodes.Add("上层协议：" + ippacket.Protocol.ToString());
 
-                if (ippacket is PacketDotNet.IPv4Packet)
+                if (ippacket is IPv4Packet)
                 {
-                    IPv4Packet ipv4 = (PacketDotNet.IPv4Packet)ippacket;
+                    IPv4Packet ipv4 = (IPv4Packet)ippacket;
                     treeView1.Nodes[count].Nodes.Add("IPv4数据包");
                     treeView1.Nodes[count].Nodes[8].Nodes.Add("校验码：" + ipv4.Checksum);
                     treeView1.Nodes[count].Nodes[8].Nodes.Add("校验码状态：" + ipv4.ValidChecksum.ToString());
@@ -396,9 +377,9 @@ namespace nps
                     treeView1.Nodes[count].Nodes[8].Nodes.Add("标识：" + ipv4.Id);
                     treeView1.Nodes[count].Nodes[8].Nodes.Add("TOS服务类型：" + ipv4.TypeOfService);
                 }
-                if (ippacket is PacketDotNet.IPv6Packet)
+                if (ippacket is IPv6Packet)
                 {
-                    IPv6Packet ipv6 = (PacketDotNet.IPv6Packet)ippacket;
+                    IPv6Packet ipv6 = (IPv6Packet)ippacket;
                     treeView1.Nodes[count].Nodes.Add("IPv6数据包：");
                     treeView1.Nodes[count].Nodes[8].Nodes.Add("流量类型：" + ipv6.TrafficClass);
                     treeView1.Nodes[count].Nodes[8].Nodes.Add("流标签：" + ipv6.FlowLabel);
@@ -416,19 +397,6 @@ namespace nps
                 treeView1.Nodes[count].Nodes.Add("序列号：" + tcpPacket.SequenceNumber);
                 treeView1.Nodes[count].Nodes.Add("ACK序列号：" + tcpPacket.AcknowledgmentNumber);
                 treeView1.Nodes[count].Nodes.Add("偏移量：" + tcpPacket.DataOffset);
-                treeView1.Nodes[count].Nodes.Add("flag：" + tcpPacket.Flags.ToString("x"));
-                int temp = treeView1.Nodes[count].Nodes.Count - 1;
-                var flags = Convert.ToString(tcpPacket.Flags, 2).PadLeft(8, '0');
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[0] + "] congestion window reduced");
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[1] + "] Ecn - echo");
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[2] + "] urgent");
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[3] + "] acknowledgement");
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[4] + "] push");
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[5] + "] reset");
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[6] + "] syn");
-                treeView1.Nodes[count].Nodes[temp].Nodes.Add("[" + flags[7] + "] fin");
-                treeView1.Nodes[count].Expand();
-                treeView1.Nodes[count].Nodes[temp].Expand();
 
             }
 
